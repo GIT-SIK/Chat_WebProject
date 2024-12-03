d672f9fc84e6c0c563239c50f958922c9793847a
GIT-SIK <xeno_s@naver.com>
2024-12-03 18:13:42 +0900

    feat (main.js) : 공통 컴포넌트 자동 등록
    문제  : 직접 접근시 component 로딩이 안되는 경우가 있음.

--- vue-study/src/main.js
+++ vue-study/src/main.js
@@ -5,12 +5,34 @@ import api from '@/utils/api'
 
 const app = createApp(App)
 
-import cbtn from '@/components/global/BaseButton.vue'
-app.component('BaseButton', cbtn)
-app.component()
 /* 공통 Axios 전역 지정 */
 app.config.globalProperties.$api = api
 
-app.use(router)
+/* 공통 뷰 전역 지정*/
+const globalComponents = import.meta.glob('@/components/global/*.vue')
 
+Object.entries(globalComponents).forEach(([path, loader]) => {
+  // path: '@/components/global/*.vue'
+  // loader: (Promise) 비동기 함수
+  loader()
+    .then((module) => {
+      // module: Component 전체 모듈 정보
+      const componentName = path
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+        .split('-')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join('')
+
+      app.component(componentName, module.default)
+      // module.default: Component 객체 정보
+    })
+    .catch((error) => {
+      console.error('Error loading global component:', error)
+    })
+})
+
+app.component()
+app.use(router)
 app.mount('#app')
