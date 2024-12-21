9bb4a135bf5b9d7165f4e724f7a5b81969c4185b
GIT-SIK <xeno_s@naver.com>
2024-12-21 23:23:15 +0900

    chore, feat : WebSocket 오류 해결을 위한 설정
    * 변경사항
       - 백엔드 포트번호 8080 -> 8081
       - WebSocket 관련 타입스크립트 의존성 추가 (@types/sockjs-client, typescript) 및 설정
       - Sockjs을 위한 global 설정

---
+++ vue-study/src/types/sockjs-client.d.ts
@@ -0,0 +1,4 @@
+declare module 'sockjs-client' {
+    const SockJS: any; // 타입을 `any`로 선언
+    export default SockJS;
+  }
--- vue-study/vite.config.js
+++ vue-study/vite.config.js
@@ -6,13 +6,16 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'global': {},
+  },
   plugins: [
     vue(),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('/src', import.meta.url))
     },
   },
 })
