494050e159641db4b2e9a80bdab41f27b51d2cee
GIT-SIK <xeno_s@naver.com>
2025-01-02 19:45:58 +0900

    feat : WebSocket 채팅 날짜 형식 추가 및 Lombok 설치
    - 이클립스에서는 Maven으로 Lombok을 가져오지 못하여 직접으로 설치하였음.

--- ws-app/src/main/java/com/example/ws_app/dto/WSMessage.java
+++ ws-app/src/main/java/com/example/ws_app/dto/WSMessage.java
@@ -1,29 +1,12 @@
 package com.example.ws_app.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
 public class WSMessage {
-    private String sender;
+	private String sender;
     private String text;
-
-    public WSMessage() {}
-
-    public WSMessage(String sender, String text) {
-        this.sender = sender;
-        this.text = text;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
+    private LocalDateTime date;
 }
