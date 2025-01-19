8bb75840d2e9f6cc259473e73c3169e0f3977946
GIT-SIK <xeno_s@naver.com>
2025-01-19 12:33:53 +0900

    feat : ModelMapper Bean 설정

---
+++ ws-back/src/main/java/com/example/ws_back/config/ModelConfig.java
@@ -0,0 +1,16 @@
+package com.example.ws_back.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelConfig {
+    /**
+     * DTO TO Entity, Entity To DTO Converter
+     */
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
