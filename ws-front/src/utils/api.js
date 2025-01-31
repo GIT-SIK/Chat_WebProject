0fe88f606e07d9d7133e94269cf2817bd196fee0
GIT-SIK <xeno_s@naver.com>
2025-01-31 19:37:07 +0900

    feat (api.js) : Axios 통신시 헤더 토큰 추가

--- ws-front/src/utils/api.js
+++ ws-front/src/utils/api.js
@@ -10,10 +10,21 @@ const axiosInst = axios.create({
   },
 })
 
-axiosInst.interceptors.response.use(
-  (response) => {
-    return response
+/* config 헤더에 토큰 추가 */
+axiosInst.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('access_token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    /* 없을 경우 토큰 없이 전송 */
+    return config
   },
+  (error) => Promise.reject(error),
+)
+
+axiosInst.interceptors.response.use(
+  (response) => response,
   (error) => {
     console.error('[api.js] API ERROR:', error.response || error.message)
     return Promise.reject(error.response || error.message)
@@ -22,11 +33,8 @@ axiosInst.interceptors.response.use(
 
 const api = {
   get: (url, config = {}) => axiosInst.get(url, config),
-
   post: (url, data = {}, config = {}) => axiosInst.post(url, data, config),
-
   put: (url, data = {}, config = {}) => axiosInst.put(url, data, config),
-
   delete: (url, config = {}) => axiosInst.delete(url, config),
 }
 
