25c4248cc4f4c658fbb014d9cd5e0e8942d32971
GIT-SIK <xeno_s@naver.com>
2025-01-31 20:43:09 +0900

    feat : 친구 목록 로직 (2) (Vue 디자인 추가)
    * 추가 및 변경사항
    - 임시 유저 이미지 추가
    - 카드 형식으로 디자인 변경

--- ws-front/src/components/FriendList.vue
+++ ws-front/src/components/FriendList.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
-    <h4>{{ userId }} 님의 친구 목록</h4>
-    <template v-for="friend in friendList" :key="friend.id">
-      <li v-if="friend.senderUserId == userId">
-        <span>{{ friend.receiverUserId }}</span>
-      </li>
-      <li v-else>
-        <span>{{ friend.senderUserId }}</span>
-      </li>
-    </template>
+    <span>{{ userId }} 님의 친구 목록</span>
+    <div class="friend-list">
+      <template v-for="friend in friendList" :key="friend.id">
+        <div class="friend-card">
+          <!-- 유저 기본 이미지 -->
+          <div class="friend-card-image">
+            <img :src="defaultUserImage" alt="유저 이미지" />
+          </div>
+          <!-- 유저 정보 -->
+          <div class="friend-card-info">
+            <div class="friend-card-id">
+              <li v-if="friend.senderUserId == userId">
+                <span>{{ friend.receiverUserId }}</span>
+              </li>
+              <li v-else>
+                <span>{{ friend.senderUserId }}</span>
+              </li>
+            </div>
+          </div>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -16,10 +29,13 @@
 import { onMounted, ref } from 'vue'
 import * as friend from '@/api/friend'
 import { useLoginStore } from '@/store/login'
+import defaultUserImage from '@/assets/default_user.png'
+
 export default {
   setup() {
     const authUser = useLoginStore()
     const friendList = ref([])
+
     const getFriendList = async () => {
       authUser.getUserInfo()
       console.log(authUser.userId)
@@ -34,7 +50,57 @@ export default {
     return {
       friendList,
       userId: authUser.userId,
+      defaultUserImage,
     }
   },
 }
 </script>
+
+<style scoped>
+/* 친구 목록 스타일 */
+.friend-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: space-evenly;
+}
+
+/* 카드 스타일 */
+.friend-card {
+  min-width: 100px;
+  max-width: 150px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+/* 유저 이미지 스타일 */
+.friend-card-image {
+  width: 40px;
+  height: 40px;
+  margin-bottom: 10px;
+}
+
+.friend-card-image img {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%; /* 둥근 이미지 */
+}
+
+/* 유저 아이디 스타일 */
+.friend-card-info {
+  font-size: 12px;
+}
+
+.friend-card-id {
+  list-style-type: none;
+  font-weight: bold;
+  color: #333;
+}
+</style>
