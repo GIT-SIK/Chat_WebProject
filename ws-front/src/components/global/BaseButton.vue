7ce247899f40b559c9d6e866ae2010b1c4fa24e7
GIT-SIK <xeno_s@naver.com>
2025-02-01 22:54:16 +0900

    feat : Vue2 → Vue3 형태로 수정

--- ws-front/src/components/global/BaseButton.vue
+++ ws-front/src/components/global/BaseButton.vue
@@ -1,67 +1,57 @@
 <template>
-  <button 
-  :class="['btn', color]" 
-  :style="btnStyle" 
-  :disabled="disabled"> <slot></slot></button>
+  <button :class="['btn', color]" :style="btnStyle" :disabled="disabled">
+    <slot></slot>
+  </button>
 </template>
 
-<script>
-export default {
-  name: 'BaseButton',
-  props: {
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    color: {
-      type: String,
-      default: '', 
-    },  
-    size: {
-      type: String,
-      default: 'medium',
-    },
-  },
-  computed: {
-    btnStyle() {
-      const size = {
-        small: { width: '80px', height: '30px' },
-        medium: { width: '120px', height: '30px' },
-        large: { width: '160px', height: '30px' },
-      };
-      return size[this.size] || size.medium;
-    },
-  },
-}
+<script setup>
+import { computed } from 'vue'
+
+defineOptions({ name: 'BaseButton' })
+
+const props = defineProps({
+  disabled: { type: Boolean, default: false },
+  color: { type: String, default: '' },
+  size: { type: String, default: 'medium' },
+})
+
+const btnStyle = computed(() => {
+  const size = {
+    small: { width: '80px', height: '30px' },
+    medium: { width: '120px', height: '30px' },
+    large: { width: '160px', height: '30px' },
+  }
+  return size[props.size] || size.medium
+})
 </script>
 
 <style scoped>
 .btn {
-    color: #444444;
-    background: #F3F3F3;
-    border: 1px #DADADA solid;
-    padding: 5px 10px;
-    border-radius: 2px;
-    font-weight: bold;
-    font-size: 9pt;
-    outline: none;
+  color: #444444;
+  background: #f3f3f3;
+  border: 1px #dadada solid;
+  padding: 5px 10px;
+  border-radius: 2px;
+  font-weight: bold;
+  font-size: 9pt;
+  outline: none;
 }
 
 .btn:hover {
-    border: 1px #c6c6c6 solid;
-    box-shadow: inset 1px 1px 1px #cdcdcd;
-    color: #333333;
-    background: #F7F7F7;
+  border: 1px #c6c6c6 solid;
+  box-shadow: inset 1px 1px 1px #cdcdcd;
+  color: #333333;
+  background: #f7f7f7;
 }
 
 .btn:active {
-    box-shadow: inset 1px 1px 1px #c0c0c0;   
+  box-shadow: inset 1px 1px 1px #c0c0c0;
 }
 
 /* Blue 스타일 */
 .btn.btn-blue {
-  color: #FFFFFF;
-  background: #007BFF;
+  color: #ffffff;
+  background: #007bff;
   border: 1px #0056b3 solid;
 }
 
@@ -76,29 +66,29 @@ export default {
 
 /* Red 스타일 */
 .btn.btn-red {
-  color: #FFFFFF;
-  background: #FF4B4B;
-  border: 1px #D32F2F solid;
+  color: #ffffff;
+  background: #ff4b4b;
+  border: 1px #d32f2f solid;
 }
 
 .btn.btn-red:hover {
-  background: #D32F2F;
-  border: 1px #A00000 solid;
+  background: #d32f2f;
+  border: 1px #a00000 solid;
 }
 
 .btn.btn-red:active {
-  background: #A00000;
+  background: #a00000;
 }
 
 /* 기본 반전 색 */
 .btn.btn-darkgray {
-  color: #F3F3F3; /* 반전된 텍스트 색상 */
-  background: #444444; /* 반전된 배경색 */
+  color: #f3f3f3;
+  background: #444444;
   border: 1px #333333 solid;
 }
 
 .btn.btn-darkgray:hover {
-  color: #F7F7F7;
+  color: #f7f7f7;
   background: #333333;
   border: 1px #222222 solid;
   box-shadow: inset 1px 1px 1px #222222;
--- ws-front/src/components/global/BaseInput.vue
+++ ws-front/src/components/global/BaseInput.vue
@@ -1,58 +1,47 @@
 <template>
-    <input
-      :type="type"
-      :style="inputStyle"
-      :placeholder="placeholder"
-      @input="$emit('update:modelValue', $event.target.value)"
-      v-bind="$attrs"
-      class="base-input"
-    />
-  </template>
-  
-  <script>
-  export default {
-    name: "BaseInput",
-    props: {
-      type: {
-        type: String,
-        default: "text",
-      },
-      placeholder: {
-        type: String,
-        default: "",
-      },
-      borderColor : {
-        type: String,
-        default: ""
-      }
-    },
-    computed: {
-      inputStyle() {
-      const borderColor = {
-        red: { border: '2px solid red' },
-        green: {border: '2px solid green' },
-        default : {border: '1px solid #aaaaaa'}
-      };
-      return borderColor[this.borderColor] || borderColor.default
-    },
-  },
-  };
-  </script>
-  
-  <style scoped>
-  .base-input {
-    width: 100%;
-    padding: 5px;
-    border: 1px solid #aaaaaa;
-    border-radius: 5px;
-    background-color: #f2f2f2;
-    color: #333333;
-    font-size: 13px;
-    outline: none;
+  <input
+    :type="type"
+    :style="inputStyle"
+    :placeholder="placeholder"
+    @input="$emit('update:modelValue', $event.target.value)"
+    v-bind="$attrs"
+    class="base-input"
+  />
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  type: { type: String, default: 'text' },
+  placeholder: { type: String, default: '' },
+  borderColor: { type: String, default: '' },
+})
+
+const inputStyle = computed(() => {
+  const borderColor = {
+    red: { border: '2px solid red' },
+    green: { border: '2px solid green' },
+    default: { border: '1px solid #aaaaaa' },
   }
-  
-  .base-input:focus {
-    border-color: #666666;
-    background-color: #e6e6e6;
-  }
-  </style>
+  return borderColor[props.borderColor] || borderColor.default
+})
+</script>
+
+<style scoped>
+.base-input {
+  width: 100%;
+  padding: 5px;
+  border: 1px solid #aaaaaa;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  color: #333333;
+  font-size: 13px;
+  outline: none;
+}
+
+.base-input:focus {
+  border-color: #666666;
+  background-color: #e6e6e6;
+}
+</style>
