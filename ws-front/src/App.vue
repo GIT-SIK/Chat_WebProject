555051fccf5f626974c0c9cad3c58d0e48030f2a
GIT-SIK <xeno_s@naver.com>
2025-02-02 00:00:23 +0900

    feat : 알람 표시 추가
    * 추가 및 변경사항
    - App.vue에서 알람을 전역에서 호출할 수 있게 설정 / inject 이용할 것.

--- ws-front/src/App.vue
+++ ws-front/src/App.vue
@@ -1,10 +1,34 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
   <header></header>
+  <ToastMessage ref="toast" />
+  <router-view :show-toast="showToast" />
   <RouterView />
 </template>
+
+<script>
+/* 알람 */
+import ToastMessage from './components/global/ToastMessage.vue'
+import { ref, provide } from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+    ToastMessage,
+  },
+  setup() {
+    const toast = ref(null)
+
+    const showToast = (message) => {
+      toast.value?.addToast(message)
+    }
+    provide('showToast', showToast)
+
+    return {
+      showToast,
+      toast,
+    }
+  },
+}
+</script>
 
 <style scoped></style>
---
+++ ws-front/src/components/global/ToastMessage.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="toast-container">
+    <TransitionGroup name="toast">
+      <div v-for="(toast, index) in toasts" :key="index" class="toast">
+        {{ toast.message }}
+      </div>
+    </TransitionGroup>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const toasts = ref([])
+
+const addToast = (message) => {
+  toasts.value.push({ message })
+
+  setTimeout(() => {
+    toasts.value.shift()
+  }, 3000)
+}
+
+// 다른 컴포넌트에서 addToast()를 호출할 수 있도록 노출
+defineExpose({ addToast })
+</script>
+
+<style scoped>
+.toast-container {
+  position: fixed;
+  top: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.toast {
+  background: #333;
+  color: #fff;
+  padding: 10px 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+  opacity: 1;
+  margin-bottom: 0 !important;
+  transition: opacity 0.5s ease-in-out;
+  display: block !important;
+}
+
+/* 애니메이션 효과 */
+.toast-enter-from,
+.toast-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+
+.toast-enter-to,
+.toast-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.toast-enter-active,
+.toast-leave-active {
+  transition: all 0.5s ease-in-out;
+}
+</style>
