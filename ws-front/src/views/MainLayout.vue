27743f17edec28b5fe0dff9b4a8817ae8505a1d4
GIT-SIK <xeno_s@naver.com>
2025-06-15 23:45:04 +0900

    feat, refactor : 네비 PATH 추가 및 코드 클리어
    * 추가 및 변경사항
    - MyAccount PATH 추가
    - MainPage 코드 클리어

--- ws-front/src/views/MainLayout.vue
+++ ws-front/src/views/MainLayout.vue
@@ -1,19 +1,15 @@
 <template>
   <v-app>
-    <v-row class="h-screen pt-5" style="background-color: #efebe9;">
-      <template v-if="$vuetify.display.mdAndUp"> 
+    <v-row class="h-screen pt-5" style="background-color: #efebe9">
+      <template v-if="$vuetify.display.mdAndUp">
         <v-col cols="1" md="1" offset-md="1" class="layout-left-container">
           <LeftNavBar />
         </v-col>
-      </template> 
+      </template>
       <template v-else>
-        <v-navigation-drawer
-        v-model="toggleStatus"
-        width = "75"
-        vertical="mini"
-        >
+        <v-navigation-drawer v-model="toggleStatus" width="75" vertical="mini">
           <div class="mt-3">
-            <LeftNavBar/>
+            <LeftNavBar />
           </div>
         </v-navigation-drawer>
       </template>
@@ -33,8 +29,8 @@
 import TopBar from '../components/TopBar.vue'
 import LeftNavBar from '../components/LeftNavbar.vue'
 import { useNavBarStore } from '@/store/navbar'
-import { useNotificationStore } from '@/store/notification' 
-import { useDisplay } from 'vuetify';
+import { useNotificationStore } from '@/store/notification'
+import { useDisplay } from 'vuetify'
 import { storeToRefs } from 'pinia'
 import { watch } from 'vue'
 
@@ -44,36 +40,34 @@ export default {
     LeftNavBar,
   },
   setup() {
-    const display = useDisplay();
-    const navBarStore = useNavBarStore();
-    const notificationStore = useNotificationStore();
-    const {toggleStatus} = storeToRefs(navBarStore)
+    const display = useDisplay()
+    const navBarStore = useNavBarStore()
+    const notificationStore = useNotificationStore()
+    const { toggleStatus } = storeToRefs(navBarStore)
 
-   /* navigation-drawer 닫힐 경우 알람창 닫기 */
+    /* navigation-drawer 닫힐 경우 알람창 닫기 */
     watch(toggleStatus, (newStatus) => {
       if (!newStatus) {
-        notificationStore.setToggleStatus(false);
+        notificationStore.setToggleStatus(false)
       }
-    });
+    })
 
-  /* display md사이즈 기준 변경시 알람창 닫기 */
+    /* display md사이즈 기준 변경시 알람창 닫기 */
     watch(display.mdAndUp, () => {
-      notificationStore.setToggleStatus(false); 
-    });
+      notificationStore.setToggleStatus(false)
+    })
 
     return {
       toggleStatus,
-      navBarStore
+      navBarStore,
     }
-  }, 
+  },
 }
 </script>
 
 <style>
-
 .layout-left-container {
   position: relative;
-  
 }
 
 .main-container {
