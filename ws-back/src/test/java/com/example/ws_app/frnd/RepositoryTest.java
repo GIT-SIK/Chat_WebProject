3a1eb464874b3d32a82106041dbaf971bd9fa826
GIT-SIK <xeno_s@naver.com>
2025-06-19 08:36:42 +0900

    test : (Friend) Repository, ServiceImpl 통합 테스트 추가

---
+++ ws-back/src/test/java/com/example/ws_app/frnd/RepositoryTest.java
@@ -0,0 +1,49 @@
+package com.example.ws_app.frnd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.ws_back.WsAppApplication;
+import com.example.ws_back.chat.ChatOracleRepository;
+import com.example.ws_back.frnd.FriendRepository;
+import com.example.ws_back.usr.UserRepository;
+
+@SpringBootTest(classes = WsAppApplication.class)
+@Transactional
+public class RepositoryTest {
+
+    @Autowired
+    private FriendRepository fr;
+    
+    @Autowired
+    private UserRepository ur;
+    
+    @Autowired
+    private ChatOracleRepository cor;
+    
+    private static final String USER_UUID = "NULL";
+    private static final String FRIEND_UUID = "NULL";
+    
+    @Test
+    public void friendRepositoryMethod() {
+
+    	
+    	/* 유저 UUID에 대한 친구 조회 (친구목록 (가공) 쿼리 사용)*/
+        List<Object[]> friends = fr.findAllByFriendWithNickname(USER_UUID);
+
+        System.out.println("친구 수: " + friends.size());
+
+        for (Object[] friend : friends) {
+             System.out.println(friend[0] + " " + friend[1] + " " + friend[2] + " " + friend[3] + " " + friend[4]);
+        }
+
+        assertNotNull(friends);
+        assertFalse(friends.isEmpty());
+    }
+}
---
+++ ws-back/src/test/java/com/example/ws_app/frnd/ServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.example.ws_app.frnd;
+
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.ws_back.WsAppApplication;
+import com.example.ws_back.chat.ChatServiceImpl;
+import com.example.ws_back.frnd.FriendInfoDto;
+import com.example.ws_back.frnd.FriendServiceImpl;
+import com.example.ws_back.usr.UserServiceImpl;
+
+@SpringBootTest(classes = WsAppApplication.class)
+public class ServiceImplTest {
+
+    @Autowired
+    private FriendServiceImpl fsi;
+    
+    @Autowired
+    private UserServiceImpl usi;
+    
+    @Autowired
+    private ChatServiceImpl csi;
+
+    private static final String USER_UUID = "NULL";
+    private static final String FRIEND_UUID = "NULL";
+    
+    @Test
+    public void friendServiceMethod() {
+    	
+    	/* 유저 UUID에 대한 친구 조회 (친구목록 (가공) 쿼리 사용)*/
+	    List<FriendInfoDto> FriendInfoDto = fsi.getUserFriendList(USER_UUID);
+
+	    FriendInfoDto.forEach(dto -> {
+            System.out.print(dto.getFriendNickname() + " ");
+            System.out.print(dto.getFriendUuid() + " ");
+            System.out.print(dto.isSender() + " ");
+            System.out.print(dto.getFriendStatus() + " ");
+            System.out.print(dto.getFriendRequestedAt() + " ");
+            System.out.print(dto.getFriendAcceptedAt());
+            System.out.println();
+        });
+    }
+}
